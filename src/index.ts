0df6240f2187e938863b2c7d91614e231080904f
Laurensvc <laurens.vancauwelaert@unbrace.be>
2019-12-31 09:25:17 +0100

    :bug: exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import ClickOutside from './components/ClickOutside';
 import Badge from './components/Badge';
+import { Button, IconButton } from './components/Button';
+import ClickOutside from './components/ClickOutside';
+import Tooltip from './components/Tooltip';
+import UnbraceThemeProvider from './components/UnbraceThemeProvider';
 
-export { ClickOutside, Badge };
+export { ClickOutside, Badge, Button, IconButton, Tooltip, UnbraceThemeProvider };
