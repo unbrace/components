8e02e52f7fe39984fc543d46120d379e223f2718
Laurensvc <laurens.vancauwelaert@unbrace.be>
2019-12-31 13:50:34 +0100

    :lable: exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,22 @@ import Badge from './components/Badge';
 import { Button, IconButton } from './components/Button';
 import ClickOutside from './components/ClickOutside';
 import Tooltip from './components/Tooltip';
+import { SortToggle, SubRow, SubTableRow, Table, TableData, TextWrap, Th, TableRow } from './components/Table';
 import UnbraceThemeProvider from './components/UnbraceThemeProvider';
 
-export { ClickOutside, Badge, Button, IconButton, Tooltip, UnbraceThemeProvider };
+export {
+  Badge,
+  Button,
+  ClickOutside,
+  IconButton,
+  SortToggle,
+  SubRow,
+  SubTableRow,
+  Table,
+  TableData,
+  TableRow,
+  TextWrap,
+  Th,
+  Tooltip,
+  UnbraceThemeProvider,
+};
