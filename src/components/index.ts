ba31f90a8876eb67909218e7a1e2173e0ca728ca
Erwin Govaerts <erwin.govaerts@unbrace.be>
2020-01-20 10:31:24 +0100

    :bug: fix merge conflicts issue

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { default as Badge } from './Badge';
 export { Button, IconButton, SubmitButton } from './Button';
 export { default as ClickOutside } from './ClickOutside';
 export * from './icons';
-export { Paginator } from './Paginator';
 export { Select } from './Select';
 export { default as TextWrap } from './TextWrap';
 export { default as Tooltip } from './Tooltip';
