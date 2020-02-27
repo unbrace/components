811b5a49da5c0ea22718d0771274987ab3def58d
Robbe <robbe.bogaerts@unbrace.be>
2020-02-27 10:33:44 +0100

    :sparkles: disabledTooltip

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { Button, IconButton, SubmitButton } from '../components/Button';
 import UnbraceThemeProvider from '../components/UnbraceThemeProvider';
-import { Close } from '../components/icons';
+import { Close, Cross } from '../components/icons';
 
 const Spacer = styled.div`
   > div {
@@ -84,6 +84,9 @@ storiesOf('Button', module).add('icon button', () => {
         </IconButton>
         <IconButton size="large" tooltip="large">
           <Close />
+        </IconButton>
+        <IconButton disabledTooltip="disabled" tooltip="tooltip" isDisabled>
+          <Cross />
         </IconButton>
       </>
     </UnbraceThemeProvider>
