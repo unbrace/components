063e7a7cd87ac4598024ac071bc10ee008947553
Erwin Govaerts <erwin.govaerts@unbrace.be>
2020-03-10 15:24:51 +0100

    :wrench: :lipstick: text improvs

--- src/theme/components/text.ts
+++ src/theme/components/text.ts
@@ -3,12 +3,36 @@ import { DefaultTheme } from '..';
 const getTextTheme = (theme: DefaultTheme) => ({
   color: {
     heading: {
-      danger: theme.palette.danger.shade5,
-      neutral: theme.palette.neutral.shade8,
-      primary: theme.palette.primary.shade5,
-      secondary: theme.palette.secondary.shade5,
-      success: theme.palette.success.shade5,
-      warning: theme.palette.warning.shade7,
+      danger: {
+        normal: theme.palette.danger.shade5,
+        medium: theme.palette.danger.shade4,
+        light: theme.palette.danger.shade3,
+      },
+      neutral: {
+        normal: theme.palette.neutral.shade8,
+        medium: theme.palette.neutral.shade5,
+        light: theme.palette.neutral.shade4,
+      },
+      primary: {
+        normal: theme.palette.primary.shade5,
+        medium: theme.palette.primary.shade4,
+        light: theme.palette.primary.shade3,
+      },
+      secondary: {
+        normal: theme.palette.secondary.shade5,
+        medium: theme.palette.secondary.shade4,
+        light: theme.palette.secondary.shade3,
+      },
+      success: {
+        normal: theme.palette.success.shade5,
+        medium: theme.palette.success.shade4,
+        light: theme.palette.success.shade3,
+      },
+      warning: {
+        normal: theme.palette.warning.shade7,
+        medium: theme.palette.warning.shade6,
+        light: theme.palette.warning.shade5,
+      },
     },
     text: {
       danger: {
