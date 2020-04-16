37f62bad596a1abb515b3086fd3ecb4971a8e11e
Robbe <robbe.bogaerts@unbrace.be>
2020-04-16 14:45:11 +0200

    :sparkles: datePickerInput

--- src/components/DatePicker/DatePickerInput.tsx
+++ src/components/DatePicker/DatePickerInput.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import DatePickerInputComponent from 'react-day-picker/DayPickerInput';
-import { DayPickerInputProps, DayModifiers } from 'react-day-picker';
+import { DayPickerInputProps, DayModifiers, DateUtils } from 'react-day-picker';
 import { Input } from '../Form/Input';
 import DatePickerWrapper from './DatePickerWrapper';
+import dateFnsFormat from 'date-fns/format';
+import dateFnsParse from 'date-fns/parse';
 
 type Props = {
   onChange?: (day: Date | undefined) => void;
 } & DayPickerInputProps;
+
+const parseDate = (str: string, format: string, locale: string) => {
+  const parsed = dateFnsParse(str, format, new Date(), ({ locale } as unknown) as { locale: Locale });
+  if (DateUtils.isDate(parsed)) {
+    return parsed;
+  }
+
+  return undefined;
+};
+
+const formatDate = (date: Date, format: string, locale: string) =>
+  dateFnsFormat(date, format, ({ locale } as unknown) as { locale: Locale });
+
+const FORMAT = 'dd/MM/yyyy';
+const PLACEHOLDER = 'DD/MM/YYYY';
 
 const DatePickerInput: React.FC<Props> = ({ onChange, ...props }: Props) => {
   const handleDaySelect = (day: Date, { selected }: DayModifiers) => {
@@ -15,13 +32,18 @@ const DatePickerInput: React.FC<Props> = ({ onChange, ...props }: Props) => {
 
   return (
     <DatePickerInputComponent
-      dayPickerProps={{
-        className: `unbrace_date-picker`,
-        onDayClick: handleDaySelect,
-      }}
+      formatDate={formatDate}
+      format={FORMAT}
+      parseDate={parseDate}
+      placeholder={PLACEHOLDER}
       component={Input}
       overlayComponent={DatePickerWrapper}
       {...props}
+      dayPickerProps={{
+        className: `unbrace_date-picker`,
+        onDayClick: handleDaySelect,
+        ...props.dayPickerProps,
+      }}
     />
   );
 };
